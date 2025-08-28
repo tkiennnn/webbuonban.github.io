8a0e76388b3ec8428347537094a3d8ffe0734b77
tkiennnn <trinhkiendz899@gmail.com>
2025-08-28 09:54:14 +0700

    update

---
+++ video.js
@@ -0,0 +1,38 @@
+// Dữ liệu mẫu phim
+const movies = [
+  {
+    title: "Sự im lặng của bầy cừu",
+    genre: ["Hành động", "Kinh dị"],
+    img: "https://i.pinimg.com/736x/58/60/3d/58603dcfa57853b22b8b60b12d3b7dbc.jpg",
+    desc: "Bộ phim nói về một đàn cừu sống trong một ngôi nhà rộng lớn cùng người chủ và chú chó chăn cừu",
+    video: "videos/video4.mp4",
+  },
+  {
+    title: "Quá khứ của kẻ phản diện",
+    genre: ["Hành động", "Hài hước"],
+    img: "https://api.memechua.com/upload/memes/jack-5-cu-khoc-meme.jpg",
+    desc: "Bộ phim nói về những người bạn với nhau, cùng nhau trải qua đắng cay ngọt bùi",
+    video: "videos/video1.mp4",
+  },
+  {
+    title: "Skibidi tolet",
+    genre: ["Kinh dị"],
+    img: "https://static.tvtropes.org/pmwiki/pub/images/skibidi_toilets.png",
+    desc: "Bộ phim nói về một đàn cừu sống trong một ngôi nhà rộng lớn cùng người chủ và chú chó chăn cừu",
+    video: "videos/video2.mp4",
+  },
+  {
+    title: "Tình yêu fi5",
+    genre: ["Tình cảm", "Hài hước"],
+    img: "https://dbk.vn/uploads/ckfinder/images/tranh-anh/anh-free-fire-9.jpg",
+    desc: "Bộ phim nói về tình cảm giữa những người chơi, họ gặp nhau trong game free fire và sau một thời gian họ nảy sinh tình cảm với nhau. Cứ như vậy họ trở thành một cặp",
+    video: "videos/video3.mp4",
+  },
+  {
+    title: "Nhạc ru ngủ cực căng",
+    genre: ["Tình cảm", "Hài hước"],
+    img: "https://inkythuatso.com/uploads/thumbnails/800/2023/02/hinh-anh-chuc-ngu-ngon-che-bua-5-10-09-16-29.jpg",
+    desc: "Nhạc giúp trẻ ngủ ngon hơn",
+    video: "videos/video5.mp4",
+  },
+];
